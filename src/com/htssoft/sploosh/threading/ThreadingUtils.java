34904abc2fa68cc901309cf93c0d3b690ea0350c
Aubrey R. Jones <aubrey@greyhelix.com>
2012-08-13 09:58:53 -0400

    Removed artificial cap on number of concurrent threads dispatched. On hyperthreading machines, acceptable performance can only be guaranteed by saturating the CPU--one thread for every advertised virtual core.

--- src/com/htssoft/sploosh/threading/ThreadingUtils.java
+++ src/com/htssoft/sploosh/threading/ThreadingUtils.java
@@ -7,9 +7,6 @@ public class ThreadingUtils {
 	 * */
 	public static int getCoreCount(){
 		int retval = Runtime.getRuntime().availableProcessors();
-		if (retval > 4){
-			retval = 4;
-		}
 		return retval;
 	}
 }
